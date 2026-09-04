8fad88960bf1a8633ae239232ba2785117bda92c
Purvesh Kolpe <purvesh.kolpe@adypu.edu.in>
2026-09-05 01:44:22 +0530

    feat: expose complete Xyris ABI surface

--- abi/include/xyris/abi/xyris_abi.h
+++ abi/include/xyris/abi/xyris_abi.h
@@ -6,5 +6,16 @@
 #include "types.h"
 #include "errors.h"
 #include "syscalls.h"
+#include "process.h"
+#include "thread.h"
+#include "memory.h"
+#include "filesystem.h"
+#include "ipc.h"
+#include "events.h"
+#include "timers.h"
+#include "devices.h"
+#include "networking.h"
+#include "security.h"
+#include "capabilities.h"
 
 #endif /* XYRIS_ABI_H */
